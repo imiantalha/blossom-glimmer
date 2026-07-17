2832274b9cb05904786bb7b082fb2f463e5835f7
imiantalha <talhaarshad427@gmail.com>
2026-07-17 12:57:16 +0500

    Monogo DB pkg installed and configured

--- config/database.php
+++ config/database.php
@@ -114,6 +114,11 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('MONGODB_URI', 'mongodb://127.0.0.1:27017'),
+            'database' => env('MONGODB_DATABASE', 'blossom_glimmer'),
+        ],
     ],
 
     /*
