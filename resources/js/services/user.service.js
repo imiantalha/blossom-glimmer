32ea43299546bd4148d197d9df98f4897c5a6e97
imiantalha <talhaarshad427@gmail.com>
2026-08-03 22:48:07 +0500

    user service

---
+++ resources/js/services/user.service.js
@@ -0,0 +1,29 @@
+import api from "../api/axios";
+
+const getUsers = (params = {}) => {
+    return api.get("/users", { params });
+}
+
+const getUser = (id) => {
+    return api.get(`/users/${id}`);
+}
+
+const createUser = (data) => {
+    return api.post("/users", data);
+}
+
+const updateUser = (id, data) => {
+    return api.put(`/users/${id}`, data);
+}
+
+const deleteUser = (id) => {
+    return api.delete(`/users/${id}`);
+}
+
+export default {
+    getUsers,
+    getUser,
+    createUser,
+    updateUser,
+    deleteUser,
+}
